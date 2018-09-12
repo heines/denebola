3fc06a303ca61a43c2859dd0e6994b4335d7415f
heine <heine.shimizu@gmail.com>
2018-09-12 16:10:37 +0900

    importをまとめた

--- redux-calculator/src/containers/CalculatorContainer.js
+++ redux-calculator/src/containers/CalculatorContainer.js
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux';
 import * as actions from '../actions';
 import NumBtn from '../components/NumBtn';
 import PointBtn from '../components/PointBtn';
-import { PlusBtn, MinusBtn, TimesBtn, DivisionBtn } from '../components/ArithmeticBtn';
+import * as abtn from '../components/ArithmeticBtn';
 import Result from '../components/Result';
 import EqualBtn from '../components/EqualBtn';
 import { ClearBtn, AClearBtn } from '../components/ClearBtn';
@@ -26,25 +26,25 @@ class CalculatorContainer extends Component {
           <NumBtn n={1} onClick={() => actions.onNumClick(1)} />
           <NumBtn n={2} onClick={() => actions.onNumClick(2)} />
           <NumBtn n={3} onClick={() => actions.onNumClick(3)} />
-          <DivisionBtn onClick={actions.onDivisionClick} />
+          <abtn.DivisionBtn onClick={actions.onDivisionClick} />
         </div>
         <div>
           <NumBtn n={4} onClick={() => actions.onNumClick(4)} />
           <NumBtn n={5} onClick={() => actions.onNumClick(5)} />
           <NumBtn n={6} onClick={() => actions.onNumClick(6)} />
-          <TimesBtn onClick={actions.onTimesClick} />
+          <abtn.TimesBtn onClick={actions.onTimesClick} />
         </div>
         <div>
           <NumBtn n={7} onClick={() => actions.onNumClick(7)} />
           <NumBtn n={8} onClick={() => actions.onNumClick(8)} />
           <NumBtn n={9} onClick={() => actions.onNumClick(9)} />
-          <MinusBtn onClick={actions.onMinusClick} />
+          <abtn.MinusBtn onClick={actions.onMinusClick} />
         </div>
         <div>
           <NumBtn n={0} onClick={() => actions.onNumClick(0)} />
           <NumBtn n={'.'} onClick={() => actions.onNumClick('.')} />
           <EqualBtn onClick={actions.onEqualClick} />
-          <PlusBtn onClick={actions.onPlusClick} />
+          <abtn.PlusBtn onClick={actions.onPlusClick} />
         </div>
       </div>
     );
